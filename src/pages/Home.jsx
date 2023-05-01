80a6e4e00aff0e840bda6fdbed83c60005198b67
Korng Samnang <korngsamnang168@gmail.com>
2023-05-01 23:41:26 +0700

    Fix search

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,11 +3,11 @@ import { useContext } from "react";
 import { AppContext } from "../context/DataProvider";
 
 const Home = () => {
-    const { posts } = useContext(AppContext);
+    const { filterLists } = useContext(AppContext);
     return (
         <div>
-            {posts.length ? (
-                posts.map(post => <EachPost post={post} key={post.id} />)
+            {filterLists.length ? (
+                filterLists.map(post => <EachPost post={post} key={post.id} />)
             ) : (
                 <p>No posts to display</p>
             )}
