62df3bd8eb722b60547a5939edc5ac876e8b1a19
Korng Samnang <korngsamnang168@gmail.com>
2023-05-02 14:13:10 +0700

    Implement data storage using local storage API

--- src/context/DataProvider.jsx
+++ src/context/DataProvider.jsx
@@ -1,11 +1,23 @@
-import { createContext, useState } from "react";
-import postData from "../../postData/posts.js";
+import { createContext, useEffect, useState } from "react";
 
 export const AppContext = createContext({});
 
 export const DataProvider = ({ children }) => {
-    const [posts, setPosts] = useState(postData);
+    const [posts, setPosts] = useState(
+        JSON.parse(localStorage.getItem("posts")) || []
+    );
     const [search, setSearch] = useState("");
+
+    useEffect(() => {
+        try {
+            localStorage.setItem("posts", JSON.stringify(posts));
+        } catch (error) {
+            //occurs when you try to add too much data to local storage and exceed its size limit.
+            if (error.name === "QuotaExceededError") {
+                alert("Storage limit exceeded");
+            }
+        }
+    }, [posts]);
 
     const filterLists = posts.filter(
         post =>
